82cf1475214b22cb38c023f8aac3c61861f27cdf
ColumbusTech <ColumbusTech@yandex.ru>
2018-03-06 15:12:22 +0300

    Math

--- Math/MathUtil.h
+++ Math/MathUtil.h
@@ -17,6 +17,15 @@ namespace Columbus
 
 	inline int32_t TruncToInt(const float Value) { return (int32_t)Value; }
 	inline float TruncToFloat(const double Value) { return (float)Value; }
+	inline int32_t FloorToInt(const float Value) { return (int32_t)floorf(Value); }
+	inline float FloorToFloat(const float Value) { return floorf(Value); }
+	inline double FloorToDouble(const double Value) { return floor(Value); }
+	inline int32_t RoundToInt(const float Value) { FloorToInt(Value + 0.5f); }
+	inline float RoundToFloat(const float Value) { FloorToFloat(Value + 0.5f); }
+	inline double RoundToDouble(const double Value) { FloorToDouble(Value + 0.5); }
+	inline int32_t CeilToInt(const float Value) { return TruncToInt(ceilf(Value)); }
+	inline float CeilToFloat(const float Value) { return ceilf(Value); }
+	inline double CeilToDouble(const double Value) { return ceil(Value); }
 
 	//e ^ Value
 	inline float Exp(const float Value) { return expf(Value); }
