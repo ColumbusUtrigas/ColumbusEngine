3e00903ebf703fbca399757ba1b1005f540a1342
ColumbusTech <ColumbusTech@yandex.ru>
2019-01-03 20:32:10 +0300

    Some

--- System/File.cpp
+++ System/File.cpp
@@ -10,7 +10,7 @@
 #else
 	// fseeko and ftello use off_t instead of long.
 	// By default, off_t is the typedef of the off32_t.
-	// To use off64_t, you should compile with the -D_FILE_OFFSET_BITS=64 flag.
+	// To use off64_t, you should set _FILE_OFFSET_BITS to 64 before including <cstdio>.
 	#define FSeek64 fseeko
 	#define FTell64 ftello
 #endif
