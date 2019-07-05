5960a26450fc0cae48ceaf04f8c2a03151538c4e
ColumbusTech <ColumbusTech@yandex.ru>
2019-07-05 21:02:18 +0300

    Light is now struct, no methods

--- Graphics/Light.h
+++ Graphics/Light.h
@@ -5,16 +5,15 @@
 namespace Columbus
 {
 
-	class Light
+	struct Light
 	{
-	public:
 		enum
 		{
 			Directional = 0,
 			Point = 1,
 			Spot = 2
 		};
-	public:
+
 		Vector3 Color = Vector3(1, 1, 1);
 		Vector3 Pos = Vector3(0, 0, 0);
 		Vector3 Dir = Vector3(1, 0, 0);
@@ -25,14 +24,6 @@ namespace Columbus
 		float Range = 10.0f;
 		float InnerCutoff = 12.5f;
 		float OuterCutoff = 17.5f;
-	public:
-		Light();
-		Light(int Type, const Vector3& Pos = Vector3(0, 0, 0));
-		Light(const char* FileName, const Vector3& Pos = Vector3(0, 0, 0));
-
-		bool Load(const char* FileName);
-		
-		~Light();
 	};
 
 }
