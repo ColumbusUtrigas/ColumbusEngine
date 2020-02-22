821e1649421abab91e1228118f0e73665219ca6c
ColumbusUtigas <ColumbusUtrigas@yandex.ru>
2020-02-22 16:48:11 +0300

    win32 native window, context and input

--- RenderAPIOpenGL/OpenGL.cpp
+++ RenderAPIOpenGL/OpenGL.cpp
@@ -61,12 +61,12 @@ namespace Columbus
 		}
 
 		glEnable(GL_MULTISAMPLE);
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_LEQUAL);
-		glEnable(GL_CULL_FACE);
-		glCullFace(GL_BACK);
+		//glEnable(GL_BLEND);
+		//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		//glEnable(GL_DEPTH_TEST);
+		//glDepthFunc(GL_LEQUAL);
+		//glEnable(GL_CULL_FACE);
+		//glCullFace(GL_BACK);
 		glEnable(GL_PROGRAM_POINT_SIZE);
 		glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
 	}
